90aa587a7ec47ebd60accac7808b2e2dcbaec894
WorldEditors <fanwang.px@gmail.com>
2021-11-09 16:06:39 +0800

    updat

--- knover/modules/prnn_cell.py
+++ knover/modules/prnn_cell.py
@@ -37,14 +37,16 @@ class PlasticRNNCell(RNNCellBase):
         super(PlasticRNNCell, self).__init__()
         std = 1.0 / math.sqrt(hidden_size)
         pl_std = 1.0e-6
-        self.weight_ih = self.create_parameter((hidden_size, input_size),
+        self.weight_ih = self.create_parameter(
+                (2 * hidden_size, input_size),
                 weight_ih_attr,
                 default_initializer=I.Uniform(-std, std))
         self.weight_hh = self.create_parameter(
-                (hidden_size, hidden_size),
+                (2 * hidden_size, hidden_size),
                 weight_hh_attr,
                 default_initializer=I.Uniform(-std, std))
-        self.bias_ih = self.create_parameter((hidden_size, ),
+        self.bias_ih = self.create_parameter(
+                (2 * hidden_size, ),
                 bias_ih_attr,
                 is_bias=True,
                 default_initializer=I.Uniform(-std, std))
@@ -72,20 +74,24 @@ class PlasticRNNCell(RNNCellBase):
         if states is None:
             states = self.get_initial_states(inputs, self.state_shape)
         w_hh, h_pre = states
-        out_h = paddle.matmul(h_pre, w_hh + self.weight_hh, transpose_y=True)
-        out_i = paddle.reshape(paddle.matmul(inputs, self.weight_ih, transpose_y=True), [0, 1, -1])
+
+        gates = paddle.matmul(inputs, self.weight_ih, transpose_y=True)
+        gates = paddle.reshape(gates, [0,1,-1])
         if self.bias_ih is not None:
-            out = out_h + out_i + self.bias_ih
-        else:
-            out = out_h + out_i
-        out = self._activation(out)
-        h_post = paddle.reshape(out, [0,1,-1])
+            gates = gates + self.bias_ih
+        gates += paddle.matmul(h_pre, self.weight_hh, transpose_y=True)
+        chunked_gates = paddle.split(gates, num_or_sections=2, axis=-1)
+        add_plastic = paddle.matmul(h_pre, w_hh, transpose_y=True)
+
+        out = self._activation(chunked_gates[0] + add_plastic)
+        s = self._gate_activation(chunked_gates[1])
+
         unit_vec = paddle.expand_as(self._unit_vec, h_pre)
         d_A = paddle.matmul(h_post, h_pre, transpose_x=True)
         d_B = paddle.matmul(h_post, unit_vec, transpose_x=True)
         d_C = paddle.matmul(unit_vec, h_pre, transpose_x=True)
 
-        w_hh_n = w_hh + d_A * self.plastic_A + d_B * self.plastic_B + d_C * self.plastic_C + paddle.expand_as(self.plastic_D, d_A)
+        w_hh_n = w_hh + s * (d_A * self.plastic_A + d_B * self.plastic_B + d_C * self.plastic_C + paddle.expand_as(self.plastic_D, d_A))
 
         return paddle.squeeze(out), (w_hh_n, out)
 
@@ -120,15 +126,15 @@ class PlasticLSTMCell(RNNCellBase):
         pl_std = 1.0e-6
 
         self.weight_ih = self.create_parameter(
-                    (4 * hidden_size, input_size),
+                    (5 * hidden_size, input_size),
                     weight_ih_attr,
                     default_initializer=I.Uniform(-std, std))
         self.weight_hh = self.create_parameter(
-                    (4 * hidden_size, hidden_size),
+                    (5 * hidden_size, hidden_size),
                     weight_hh_attr,
                     default_initializer=I.Uniform(-std, std))
         self.bias_ih = self.create_parameter(
-                    (4 * hidden_size, ),
+                    (5 * hidden_size, ),
                     bias_ih_attr,
                     is_bias=True,
                     default_initializer=I.Uniform(-std, std))
@@ -163,12 +169,12 @@ class PlasticLSTMCell(RNNCellBase):
         gates += paddle.matmul(pre_hidden, self.weight_hh, transpose_y=True)
 
         add_plastic = paddle.matmul(pre_hidden, w_hh, transpose_y=True)
-        chunked_gates = paddle.split(gates, num_or_sections=4, axis=-1)
-
+        chunked_gates = paddle.split(gates, num_or_sections=5, axis=-1)
 
         i = self._gate_activation(chunked_gates[0])
         f = self._gate_activation(chunked_gates[1])
         o = self._gate_activation(chunked_gates[3] + add_plastic)
+        s = self._gate_activation(chunked_gates[4])
         c = f * pre_cell + i * self._activation(chunked_gates[2])
         h = o * self._activation(c)
 
@@ -178,7 +184,8 @@ class PlasticLSTMCell(RNNCellBase):
         d_B = paddle.matmul(h_post, unit_vec, transpose_x=True)
         d_C = paddle.matmul(unit_vec, pre_hidden, transpose_x=True)
 
-        w_hh_n = w_hh + d_A * self.plastic_A + d_B * self.plastic_B + d_C * self.plastic_C + paddle.expand_as(self.plastic_D, d_A)
+        w_hh_n = w_hh + s * (d_A * self.plastic_A + d_B * self.plastic_B 
+                + d_C * self.plastic_C + paddle.expand_as(self.plastic_D, d_A))
 
         return paddle.squeeze(h), (w_hh_n, h, c)
 
--- projects/PRNN/run.sh
+++ projects/PRNN/run.sh
@@ -1,32 +1,34 @@
 RUN_FILE=$0
 PROJECT_DIR=$(readlink -f "$(dirname "$0")")
 ROOTPATH=$(readlink -f "$(dirname "$0")/../../")
-nohup bash $ROOTPATH/scripts/local/job.sh $RUN_FILE > $PROJECT_DIR/run/log.train.prnn.512 &
+nohup bash $ROOTPATH/scripts/local/job.sh $RUN_FILE > $PROJECT_DIR/run/log.train.rnn.512 &
 
 # job settings
 job_script="$ROOTPATH/scripts/single_gpu/train.sh"
 
 # task settings
+export CUDA_VISIBLE_DEVICES=0
 model=RecursiveModels
 task=DialogGeneration
 
 vocab_path="$ROOTPATH/package/dialog_en/vocab.txt"
 spm_model_file="$ROOTPATH/package/dialog_en/spm.model"
-train_file="$ROOTPATH/data/example/train_filelist"
-valid_file="$ROOTPATH/data/example/valid_filelist"
-data_format="raw"
-file_format="filelist"
-config_path="$ROOTPATH/projects/PRNN/prnn.json"
+train_file="$ROOTPATH/data/example/train_wow.tokenized.tsv"
+valid_file="$ROOTPATH/data/example/val_wow_seen_unseen.tokenized.tsv"
+data_format="tokenized"
+#raw, tokenized, numerical
+file_format="file"
+config_path="$ROOTPATH/projects/PRNN/rnn.json"
 
 # training settings
 in_tokens="false"
-batch_size=32
-lr=1e-5
+batch_size=16
+lr=1e-4
 warmup_steps=500
 weight_decay=0.01
 num_epochs=10000
 
-train_args="--max_knowledge_len 128 --max_src_len 256 --max_tgt_len 128 --max_seq_len 512"
+train_args="--max_knowledge_len 128 --max_src_len 256 --max_tgt_len 128 --max_seq_len 512 --knowledge_position pre_src --sort_pool_size 0"
 
 log_steps=10
 validation_steps=1000
@@ -34,4 +36,3 @@ save_steps=10000
 
 log_dir="./log"
 save_path="./output"
-export CUDA_VISIBLE_DEVICES=0
